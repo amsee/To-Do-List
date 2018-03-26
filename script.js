1e6b6552521831c0267fb5d2291f0bdcadefcef8
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-03-26 19:36:35 -0400

    To-Do Webpage

--- script.js
+++ script.js
@@ -1,176 +1,56 @@
 // Add JavaScript File
+// array = todo list
+// add/delete to that array using certain functions
+// implement that into js
 
+alert("Hi 👻"); 
+alert("⬇ add a new item to the to-do list below ⬇");
 
-// * initialize array  
+// * initialize array 
 
-/* var fruit = [
-    "pear", 
-    "apple", 
-    "peach", 
-    "strawberry", 
-    "kiwi"
-];
+// Set some dummy data
+var fruits = ["Apples", "Oranges", "Peaches"];
 
-// * apend new value to array
-fruit.push("dragonfruit")
-
-// // Create a "close" button and append it to each list item
-
-var liTags = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < fruit.length; i++) {
-//   alert(fruit[0])
-  console.log(fruit[i]);
+// function that runs when the body element loads up
+function windowLoads() {
+    loadUnorderedList();
 }
 
-*/
+// Loads the fruits list to the site
+function loadUnorderedList() {
+    var ul = document.getElementById("fruits");
 
-function splitSelector(selector)
-{
-  if (selector.indexOf(",") == -1)
-    return [selector];
+    // https://www.w3schools.com/jsref/met_node_appendchild.asp
+    
+    // For every fruit in the fruits array...
+    for (var i = 0; i < fruits.length; i++) {
 
-  var selectors = [];
-  var start = 0;
-  var level = 0;
-  var sep = "";
+        // Create an li tag
+        var node = document.createElement("LI");                 // Create a <li> node
 
-  for (var i = 0; i < selector.length; i++)
-  {
-    var chr = selector[i];
+        // Create a textnode that contains fruits[i] -> the current fruit element
+        var textnode = document.createTextNode(fruits[i]);         
 
-    if (chr == "\\")        // ignore escaped characters
-      i++;
-    else if (chr == sep)    // don't split within quoted text
-      sep = "";             // e.g. [attr=","]
-    else if (sep == "")
-    {
-      if (chr == '"' || chr == "'")
-        sep = chr;
-      else if (chr == "(")  // don't split between parentheses
-        level++;            // e.g. :matches(div,span)
-      else if (chr == ")")
-        level = Math.max(0, level - 1);
-      else if (chr == "," && level == 0)
-      {
-        selectors.push(selector.substring(start, i));
-        start = i + 1;
-      }
+        // Append the textnode to the li tag - <li>TextNode</li> is essentially what's happening
+        node.appendChild(textnode);                              
+
+        // Append the created <li>TextNode</li> to the <ul></ul> list from the page -> <ul><li>TextNode</li></ul>
+        ul.appendChild(node);
     }
-  }
-
-  selectors.push(selector.substring(start));
-  return selectors;
 }
 
-function CSSPropertyFilters(window, getFiltersFunc, addSelectorsFunc)
-{
-  this.window = window;
-  this.getFiltersFunc = getFiltersFunc;
-  this.addSelectorsFunc = addSelectorsFunc;
-}
+// Gets the value in the input box and adds it to the fruits array
+function addInputToFruits() {
 
-CSSPropertyFilters.prototype = {
-  stringifyStyle: function(style)
-  {
-    var styles = [];
-    for (var i = 0; i < style.length; i++)
-    {
-      var property = style.item(i);
-      var value    = style.getPropertyValue(property);
-      var priority = style.getPropertyPriority(property);
-      styles.push(property + ": " + value + (priority ? " !" + priority : "") + ";");
-    }
-    styles.sort();
-    return styles.join(" ");
-  },
-
-  isSameOrigin: function(stylesheet)
-  {
-    try
-    {
-      return new URL(stylesheet.href).origin == this.window.location.origin;
-    }
-    catch (e)
-    {
-      // Invalid URL, assume that it is first-party.
-      return true;
-    }
-  },
-
-  findSelectors: function(stylesheet, selectors, filters)
-  {
-    // Explicitly ignore third-party stylesheets to ensure consistent behavior
-    // between Firefox and Chrome.
-    if (!this.isSameOrigin(stylesheet))
-      return;
-
-    var rules = stylesheet.cssRules;
-    if (!rules)
-      return;
-
-    for (var i = 0; i < rules.length; i++)
-    {
-      var rule = rules[i];
-      if (rule.type != rule.STYLE_RULE)
-        continue;
-
-      var style = this.stringifyStyle(rule.style);
-      for (var j = 0; j < this.patterns.length; j++)
-      {
-        var pattern = this.patterns[j];
-        var regexp = pattern.regexp;
-
-        if (typeof regexp == "string")
-          regexp = pattern.regexp = new RegExp(regexp);
-
-        if (regexp.test(style))
-        {
-          var subSelectors = splitSelector(rule.selectorText);
-          for (var k = 0; k < subSelectors.length; k++)
-            selectors.push(pattern.prefix + subSelectors[k] + pattern.suffix);
-          filters[pattern.text] = true;
-        }
-      }
-    }
-  },
-
-  addSelectors: function(stylesheets)
-  {
-    var selectors = [];
-    var filters = {};
-    for (var i = 0; i < stylesheets.length; i++)
-      this.findSelectors(stylesheets[i], selectors, filters);
-    this.addSelectorsFunc(selectors, Object.keys(filters));
-  },
-
-  onLoad: function(event)
-  {
-    var stylesheet = event.target.sheet;
-    if (stylesheet)
-      this.addSelectors([stylesheet]);
-  },
-
-  selector.push("")
-
-  load: function(callback)
-  {
-    this.getFiltersFunc(function(patterns)
-    {
-      this.patterns = patterns;
-      callback();
-    }.bind(this));
-  },
-
-  apply: function()
-  {
-    if (this.patterns.length > 0)
-    {
-      var document = this.window.document;
-      this.addSelectors(document.styleSheets);
-      document.addEventListener("load", this.onLoad.bind(this), true);
-    }
-  }
-};
-
-
+    // get the data typed from the input box
+    var fruitBoxThingyValue = document.getElementById("fruitsInputBox").value;
+    
+    // Add the value from the input box to the fruits array
+    fruits.push(fruitBoxThingyValue);
+    
+    // Check the contents of fruits
+    console.log(fruits);
+    var node = document.createElement("LI");             
+    var textnode = document.createTextNode(fruitBoxThingyValue);    
+    node.appendChild(textnode);                       
+    document.getElementById("fruits").appendChild(node);  
