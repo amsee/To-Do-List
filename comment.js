46569a2bea32c16c76652628bde15b36555c8d9d
Amy Wong <amywong@eduroam-198-96-32-195.eduroam.yorku.ca>
2018-03-29 18:35:47 -0400

    To-Do Webpage

---
+++ comment.js
@@ -0,0 +1,29 @@
+
+/*
+Check if num is greater than 0.
+
+1. Check if num == 0
+2. If not, check if num < 0
+3. Else num > 0
+*/
+//// ------------------------------------------------------------------------ //// 
+/*
+Check if a string has more than 10 characters
+
+1. Grab the string and put it in variable "str"
+2. Get the length of that string by using str.length to store the number in a variable length
+3. Check if the length > 10. If not, return True
+*/
+
+// ? 1. Create an array
+// ? 2. Create a function to add to array
+    // * 3. Get element from id="textbox"
+    // * 4. Get value from id="textbox" and put in the variable value
+    // * 5. Add (push) value to array
+// ? 6. Add to html
+    // * 7. Create a <li> node
+    // * 8. Create a text node
+// ? 9. Create <li> tag
+    // * 10. Input text in new <li> tag
+// ? 11. Append <li> tag 
+    // * 12. Add <li> tags to <ul>
