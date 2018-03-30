6fd2939817f8e08f4e630dc7cb5ef6cee7738fe0
Amy Wong <amywong@Amys-MacBook-Pro.local>
2018-03-30 17:53:17 -0400

    To-Do Webpage

---
+++ codeComment.js
@@ -0,0 +1,27 @@
+// todo: CREATE ARRAY
+
+// * Create list
+// * Assign array to variable
+
+var fruits = ["Apples", "Peaches", "Pear"];
+
+// todo: ADD TO ARRAY
+
+// * Create function to add variable to array
+
+function addInput(){
+    // * Create variable to get element through id="textbox" 
+    // ? Pull element through the id called "textbox"
+    var fruitList = document.getElementById("textbox");
+    // * Create variable to add value to array
+    // ? Add to array after data input
+    var value = fruitList(value);
+    // * Append (push) variable to array
+    // ? When "Add To-Do" button is clicked, run add addInput() function
+    fruits.push(value);
+}
+
+// todo: ADD TO HTML 
+
+
+
